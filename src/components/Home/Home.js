3ba15eab16d68531f37639f00b154df3911bedbb
Ishant kumawat <ishantkumawat334445@gmail.com>
2024-05-13 15:20:30 +0530

    commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,15 @@ import './Home.css'
 const Home = () => {
     return (
         <div className='containerhome'>
-            <p className='main'>
+            <div className='main'>
                 <p id='firstpara'>Hi, my name is</p>
                 <p id='secondpara'>Ishant Kumawat.</p>
                 <p id='thirdpara'>I build things for the web.</p>
                 <p id='forthpara'>I'm a Software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on
                     building accessible, human-centered products at <span id='childforth'>Upstatement</span> .</p>
-                    <div className='homebtn' ><button id='homebtn'>Check out My cousre !</button>
+                    <div className='homebtn' ><button id='homebtn'>My Projects Preview !</button>
                     <span id='homespan'></span></div>    
-            </p>
+            </div>
            
         </div>
     )
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from 'react';
 import './navbar.css';
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
+import IshantResume from '../../assests/img/Ishant_Resume.pdf_page-0001.jpg';
 
 const Navbar = () => {
+  const openPDF = () => {
+    window.open(IshantResume, '_blank'); 
+  };
+
   return (
-  <nav className='navbar'>
-   <div className='navmenu'>
-   <Link className='navlist' ><p id='point'>01.</p>About</Link>
-   <Link className='navlist'><p id='point'>02.</p>Experience</Link>
-   <Link className='navlist'><p id='point'>03.</p>Work</Link>
-   <Link className='navlist'><p id='point'>04.</p>Contact</Link>
-   </div>
-   <div id='navbtn'>
-   <button className='navbtn'>Resume</button>
-   <span id='navspan'></span>
-   </div>
-    
-  </nav>
-  )
+    <nav className='navbar'>
+      <div className='navmenu'>
+        <Link className='navlist'><p id='point'>01.</p>About</Link>
+        <Link className='navlist'><p id='point'>02.</p>Experience</Link>
+        <Link className='navlist'><p id='point'>03.</p>Work</Link>
+        <Link className='navlist'><p id='point'>04.</p>Contact</Link>
+      </div>
+      <div id='navbtn'>
+        <button className='navbtn' onClick={openPDF}>Resume</button>
+        <span id='navspan'></span>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
